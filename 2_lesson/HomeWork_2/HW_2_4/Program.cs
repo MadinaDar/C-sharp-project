968459433e7af0653bf632be7c9371e46595e8ac
madina <madinad@gmail.com>
2022-07-05 23:23:45 +0600

    Homework2

---
+++ 2_lesson/HomeWork_2/HW_2_4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и
+// проверяет, является ли этот день выходным.
+
+void Weekend(int num)
+{
+    string text = "No";
+    if (num == 6 || num == 7) text = "Yes";
+    Console.WriteLine($"{num} -> {text}");
+}
+Weekend (3);
+Weekend (7);
+Weekend (1);
